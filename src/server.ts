1c851fbd72ac56704de194708497606174ae1a71
Chivan9406 <ivan9406@hotmail.com>
2026-06-25 12:36:56 -0600

    Add user model and authentication routes; enhance CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import projectRoutes from './routes/projectRoutes'
 import cors from 'cors'
 import { corsConfig } from './config/cors'
 import morgan from 'morgan'
+import authRoutes from './routes/authRoutes'
 
 dotenv.config()
 connectDb()
@@ -17,6 +18,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', projectRoutes)
 
 export default app
