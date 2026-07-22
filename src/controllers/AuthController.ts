f387ea8ab8f9551ea64e0fa6e6cd33fc625c7306
Chivan9406 <ivan9406@hotmail.com>
2026-07-22 15:26:01 -0600

    Add account confirmation feature with email verification

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
-import User from '../models/User'
+import User, { IUser } from '../models/User'
 import { hashPassword } from '../utils/auth'
+import Token from '../models/Token'
+import { generateToken } from '../utils/token'
+import { AuthEmail } from '../emails/AuthEmail'
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -16,10 +19,41 @@ export class AuthController {
 
       const user = new User(req.body)
       user.password = await hashPassword(password)
-      await user.save()
+
+      const token = new Token()
+      token.token = generateToken()
+      token.user = user._id
+
+      await AuthEmail.sendConfirmationEmail({
+        email: user.email,
+        name: user.name,
+        token: token.token,
+      })
+
+      await Promise.allSettled([user.save(), token.save()])
       res.send('Cuenta creada. Revisa tu email para verificar tu cuenta')
     } catch (error) {
       res.status(500).json({ error: 'Error al crear la cuenta' })
+    }
+  }
+
+  static confirmAccount = async (req: Request, res: Response) => {
+    try {
+      const { token } = req.body
+      const tokenExists = await Token.findOne({ token })
+
+      if (!tokenExists) {
+        const error = new Error('Token no válido')
+        return res.status(401).json({ error: error.message })
+      }
+
+      const user: IUser = await User.findById(tokenExists.user)
+      user.confirmed = true
+
+      await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+      res.send('Cuenta confirmada exitosamente')
+    } catch (error) {
+      res.status(500).json({ error: 'Error al confirmar la cuenta' })
     }
   }
 }
